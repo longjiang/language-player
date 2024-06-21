834d6d4b0ee66acc1d623ddd1193f67095ec30ee
Jiang Long <longjiang2005@gmail.com>
2024-06-21 00:16:14 -0700

    Initial commit

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,81 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+import { Swatches } from './Swatches';  // Assuming Swatches are defined in the same file or imported appropriately
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    text: {
+      primary: Swatches.neutral[0],
+      secondary: Swatches.neutral[300],
+    },
+    background: {
+      primary: Swatches.neutral[0],
+      secondary: Swatches.neutral[50],
+    },
+    brand: {
+      primary: Swatches.primary[600],
+      secondary: Swatches.success[600],
+      tertiary: Swatches.warning[600],
+      alternate: Swatches.alert[600],
+    },
+    accent: {
+      accent1: Swatches.primary[400],
+      accent2: Swatches.success[400],
+      accent3: Swatches.warning[400],
+      accent4: Swatches.alert[400],
+    },
+    semantic: {
+      success: Swatches.success[600],
+      error: Swatches.alert[600],
+      warning: Swatches.warning[600],
+      info: Swatches.neutral[300],
+    },
+    stroke: {
+      primary: Swatches.neutral[200],
+      secondary: Swatches.neutral[100],
+    },
+    link: {
+      primary: Swatches.primary[400],
+      secondary: Swatches.primary[700],
+    },
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+    text: {
+      primary: Swatches.neutral[0],
+      secondary: Swatches.neutral[200],
+    },
+    background: {
+      primary: Swatches.neutral[900],
+      secondary: Swatches.neutral[600],
+    },
+    brand: {
+      primary: Swatches.primary[500],
+      secondary: Swatches.success[500],
+      tertiary: Swatches.warning[500],
+      alternate: Swatches.alert[500],
+    },
+    accent: {
+      accent1: Swatches.primary[700],
+      accent2: Swatches.success[700],
+      accent3: Swatches.warning[700],
+      accent4: Swatches.alert[700],
+    },
+    semantic: {
+      success: Swatches.success[600],
+      error: Swatches.alert[500],
+      warning: Swatches.warning[500],
+      info: Swatches.neutral[300],
+    },
+    stroke: {
+      primary: Swatches.neutral[300],
+      secondary: Swatches.neutral[400],
+    },
+    link: {
+      primary: Swatches.primary[300],
+      secondary: Swatches.primary[600],
+    },
+  }
 };
+
+export default Colors;
