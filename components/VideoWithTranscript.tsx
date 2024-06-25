b452b25040700d530b41a6629a2d1760ebd36cf2
Jiang Long <longjiang2005@gmail.com>
2024-06-25 00:28:46 -0700

    Can maximize a mini player by tapping.

--- components/VideoWithTranscript.tsx
+++ components/VideoWithTranscript.tsx
@@ -8,7 +8,7 @@ import { VideoControlBar } from "./VideoControlBar";
 import { SyncedTranscript } from "./SyncedTranscript";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Ionicon from "react-native-vector-icons/Ionicons";
-import { router } from "expo-router";
+import { router, Link } from "expo-router";
 import { Dimensions } from "react-native";
 import { VideoWithTranscriptProvider } from "@/contexts/VideoWithTranscriptContext";
 import { YouTubeVideo as YouTubeVideoType } from "@/types";
@@ -19,10 +19,14 @@ import { Swatches } from "@/constants/Swatches";
 import { ThemedText } from "./ThemedText";
 import { formatSeconds } from "@/src/utils";
 import { useVideoPlayer } from "@/contexts/VideoPlayerContext";
+import {
+  GestureHandlerRootView,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 
 interface VideoWithTranscriptProps {
-  isMini: boolean
-  showHeader: boolean
+  isMini: boolean;
+  showHeader: boolean;
 }
 
 export const VideoWithTranscript: React.FC<VideoWithTranscriptProps> = ({
@@ -33,15 +37,15 @@ export const VideoWithTranscript: React.FC<VideoWithTranscriptProps> = ({
   const videoHeight = screenWidth * 0.5625; // 16:9 aspect ratio
   const primaryBrandColor = useThemeColor({}, "primaryBrand");
 
-  const { video, playVideo, updatePlayVideo, currentTime, startTime } = useVideoWithTranscriptContext();
+  const { video, playVideo, updatePlayVideo, currentTime, startTime } =
+    useVideoWithTranscriptContext();
   const { closePlayer } = useVideoPlayer();
 
   function removeTextInBrackets(text) {
     // Regular expression to match content inside various brackets
     const regex = /[\(\[\{［【｛].*?[\)\]\}］】｝]/g;
-    return text.replace(regex, '');
+    return text.replace(regex, "");
   }
-
 
   if (!video) {
     return null;
@@ -51,27 +55,29 @@ export const VideoWithTranscript: React.FC<VideoWithTranscriptProps> = ({
     <View>
       {!isMini && (
         <View>
-          {(showHeader && <SafeAreaView style={styles.header}>
-            <View>
-              <ThemedButton
-                type="ghost"
-                trailingIcon={<Icon name="chevron-down" />}
-                onPress={() => router.push("../")}
-              />
-            </View>
-            <View style={{ flexDirection: "row" }}>
-              <ThemedButton
-                type="ghost"
-                trailingIcon={<Icon name="text-long" />}
-                onPress={() => router.push("/(tabs)/(media)/youtube-video")}
-              />
-              <ThemedButton
-                type="ghost"
-                trailingIcon={<Icon name="cog-outline" />}
-                onPress={() => router.push("/(tabs)/(media)/youtube-video")}
-              />
-            </View>
-          </SafeAreaView>)}
+          {showHeader && (
+            <SafeAreaView style={styles.header}>
+              <View>
+                <ThemedButton
+                  type="ghost"
+                  trailingIcon={<Icon name="chevron-down" />}
+                  onPress={() => router.push("../")}
+                />
+              </View>
+              <View style={{ flexDirection: "row" }}>
+                <ThemedButton
+                  type="ghost"
+                  trailingIcon={<Icon name="text-long" />}
+                  onPress={() => router.push("/(tabs)/(media)/youtube-video")}
+                />
+                <ThemedButton
+                  type="ghost"
+                  trailingIcon={<Icon name="cog-outline" />}
+                  onPress={() => router.push("/(tabs)/(media)/youtube-video")}
+                />
+              </View>
+            </SafeAreaView>
+          )}
           <View style={styles.fullPlayerContainer}>
             <YouTubeVideo
               youtubeId={video.youtube_id}
@@ -94,14 +100,24 @@ export const VideoWithTranscript: React.FC<VideoWithTranscriptProps> = ({
               startTime={startTime}
             />
           </View>
-          <View style={styles.miniPlayerVideoInfo}>
-            <ThemedText style={styles.miniPlayerVideoTitle} numberOfLines={1} type="defaultBold">
-              {removeTextInBrackets(video.title)}
-            </ThemedText>
-            <ThemedText style={styles.miniPlayerVideoSubTitle} numberOfLines={1} type="small">
-              {formatSeconds(currentTime)}
-            </ThemedText>
-          </View>
+          <Link href={`/video/youtube/${video.youtube_id}`} style={{ marginLeft: 10, flex: 1 }}>
+            <View style={styles.miniPlayerVideoInfo}>
+              <ThemedText
+                style={styles.miniPlayerVideoTitle}
+                numberOfLines={1}
+                type="defaultBold"
+              >
+                {removeTextInBrackets(video.title)}
+              </ThemedText>
+              <ThemedText
+                style={styles.miniPlayerVideoSubTitle}
+                numberOfLines={1}
+                type="small"
+              >
+                {formatSeconds(currentTime)}
+              </ThemedText>
+            </View>
+          </Link>
           <View style={styles.miniPlayerControlsContainer}>
             <Ionicon
               name={playVideo ? "pause" : "play"}
@@ -126,9 +142,7 @@ export const VideoWithTranscript: React.FC<VideoWithTranscriptProps> = ({
 const styles = StyleSheet.create({
   header: { flexDirection: "row", justifyContent: "space-between" },
   miniPlayerVideoInfo: {
-    flex: 1,
-    marginLeft: 10,
-    overflow: 'hidden', // Ensures overflow is hidden
+    overflow: "hidden", // Ensures overflow is hidden
   },
   miniPlayerVideoTitle: { fontSize: 14, color: Swatches.neutral[0] },
   miniPlayerVideoSubTitle: { fontSize: 12, color: Swatches.neutral[0] },
