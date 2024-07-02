16e101f5d68c0e2a617324b4e21b2ccb925c61c3
Jiang Long <longjiang2005@gmail.com>
2024-07-02 12:39:53 -0700

    Subs search can handle multiple languages.

--- components/SubsSearch.tsx
+++ components/SubsSearch.tsx
@@ -1,21 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, ScrollView, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
 import axios from 'axios';  // You need to install axios for HTTP requests
-import { YouTubeVideo } from './YouTubeVideo';
-import { VideoWithTranscript } from './VideoWithTranscript';
 import { VideoWithTranscriptProvider } from '@/contexts/VideoWithTranscriptContext';
-import { ThemedText } from './ThemedText';
 import { SubsSearchResults } from './SubsSearchResults';
-import { SubsSearchResultsList } from "@/components/SubsSearchResultsList";
-import { parse } from '@babel/core';
 import { parseSubtitles } from '@/src/subs';
-import RBSheet from 'react-native-raw-bottom-sheet';
-import { useThemeColor } from "@/hooks/useThemeColor";
 import { PYTHON_SERVER } from '@/src/api/python'
+import { useLanguage } from '@/contexts/LanguageContext';
 
 export const SubsSearch = ({ term }: { term: string }): JSX.Element => {
   const [results, setResults] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const { l2Lang } = useLanguage()
+  if (!l2Lang) return <Text>Loading...</Text>
 
   useEffect(() => {
     if (term) {
@@ -26,7 +22,7 @@ export const SubsSearch = ({ term }: { term: string }): JSX.Element => {
   const fetchResults = async (searchTerm: string): Promise<void> => {
     setLoading(true);
     try {
-      const response = await axios.get(`${PYTHON_SERVER}/subs-search?l2=zh&terms=${searchTerm}&limit=50&tv_show=nnull&sort=-views`);
+      const response = await axios.get(`${PYTHON_SERVER}/subs-search?l2=${l2Lang.code}&terms=${searchTerm}&limit=50&tv_show=nnull&sort=-views`);
       const data = response.data;
       data.forEach((item: any) => {
         item.subs_l1 = parseSubtitles(item.subs_l2);
@@ -41,7 +37,7 @@ export const SubsSearch = ({ term }: { term: string }): JSX.Element => {
   };
 
   return (
-    <View style={styles.container}>
+    <View>
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
@@ -57,8 +53,3 @@ const handleSubtitlePress = (time) => {
   // You'll need to integrate with your video player to seek to this time
   console.log('Seek video to:', time);
 };
-
-const styles = StyleSheet.create({
-  container: {
-  },
-});
