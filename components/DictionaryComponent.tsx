d4986c3302abad282656dd60083209ee596140e3
Jiang Long <longjiang2005@gmail.com>
2024-07-02 04:02:49 -0700

    useTraditional should affect dictionary search list

--- components/DictionaryComponent.tsx
+++ components/DictionaryComponent.tsx
@@ -8,16 +8,25 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { router } from 'expo-router';
 import { DictionaryEntry } from '@/src/dictionary-types';
 import { debounce } from 'lodash';
+import { useLanguage } from '@/contexts/LanguageContext';
+import { useSettings } from '@/contexts/SettingsContext';
 
 export const DictionaryComponent = () => {
     const [query, setQuery] = useState('');
     const [results, setResults] = useState<DictionaryEntry[]>([]);
     const { dictionary } = useDictionary();
-
+    const { settings } = useSettings(); // Use the settings from context
+    const { l2Lang } = useLanguage(); // Use the language from context
+    if (!l2Lang) return null;
+    
     const handleSearch = async (text: string) => {
         setQuery(text);
         if (dictionary) setResults((await dictionary.search(text)).slice(0, 50));
     };
+
+    // if l2Lang.code is 'zh' and settings.useTraditional is true, then switch head and alternate keys
+    const headKey = l2Lang.code === 'zh' && settings.useTraditional ? 'alternate' : 'head';
+    const alternateKey = l2Lang.code === 'zh' && settings.useTraditional ? 'head' : 'alternate';
 
     return (
         <View>
@@ -32,8 +41,8 @@ export const DictionaryComponent = () => {
                     <View key={index} style={{marginTop: 16}}>
                         <TouchableOpacity onPress={() => router.navigate(`/dictionary/word/${entry.id}`)}>
                           <ThemedText>
-                              <ThemedText type="title" level={entry.level}>{entry.head}</ThemedText>
-                              <ThemedText type="default" variant="secondary"> {entry.alternate}</ThemedText>
+                              <ThemedText type="title" level={entry.level}>{entry[headKey]}</ThemedText>
+                              <ThemedText type="default" variant="secondary"> {entry[alternateKey]}</ThemedText>
                               <ThemedText type="defaultBold"> {entry.pronunciation}</ThemedText>
                               <ThemedText type="default"> • {entry.definitions.join('; ')}</ThemedText>
                           </ThemedText>
