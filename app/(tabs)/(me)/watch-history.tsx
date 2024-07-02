0982eb39eecd46ff5a8c3714f9089b23843acc21
Jiang Long <longjiang2005@gmail.com>
2024-07-02 13:25:33 -0700

    Clean up watch history page.

--- app/(tabs)/(me)/watch-history.tsx
+++ app/(tabs)/(me)/watch-history.tsx
@@ -1,19 +1,11 @@
 // @/app/watch-history.tsx
 import React, { useState } from "react";
-import { StyleSheet } from "react-native";
-import { ThemedButton } from "@/components/ThemedButton";
 import { ThemedScreen } from "@/components/ThemedScreen";
-import Icon from "react-native-vector-icons/MaterialIcons";
 import { router } from "expo-router";
 import { YouTubeVideoList } from "@/components/YouTubeVideoList";
-import videoData from '@/data/recommended-videos.json'; // Importing the JSON data
+import videoData from '@/data/recommended-videos.json'; // Example data
 
 const WatchHistoryScreen = () => {
-  const [code, setCode] = useState("");
-
-  const onSelect = (value: string) => {
-    console.log('Selected:', value);
-  }
 
   return (
     <ThemedScreen
@@ -25,18 +17,5 @@ const WatchHistoryScreen = () => {
     </ThemedScreen>
   );
 };
-
-const styles = StyleSheet.create({
-  item: {
-    padding: 16,
-  },
-  instructions: {
-    marginBottom: 20,
-  },
-  button: {
-    marginTop: 20,
-    marginBottom: 110,
-  },
-});
 
 export default WatchHistoryScreen;
