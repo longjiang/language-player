0cd349aa89eb6a3aa41b508ae2521533f24e1e11
Jiang Long <longjiang2005@gmail.com>
2024-07-02 21:10:10 -0700

    Load language progress.

--- contexts/UserDataContext.tsx
+++ contexts/UserDataContext.tsx
@@ -12,17 +12,28 @@ interface SavedWords {
   [langCode: string]: Word[];
 }
 
+interface Progress {
+  [langCode: string]: {
+    level: string;
+    time: number;
+  };
+}
+
 interface UserData {
   id: string;
   saved_words: SavedWords;
+  progress: Progress;
 }
 
 interface UserDataContextProps {
   userData: UserData | null;
   savedWords: SavedWords;
+  progress: Progress;
   hasSavedWord: (langCode: string, wordId: string) => boolean;
   saveWord: (langCode: string, wordId: string) => Promise<void>;
   removeSavedWord: (langCode: string, wordId: string) => Promise<void>;
+  getProgress: (langCode: string) => { level: string; time: number } | undefined;
+  updateProgress: (langCode: string, newProgress: { level: string; time: number }) => Promise<void>;
 }
 
 interface UserDataProviderProps {
@@ -34,7 +45,8 @@ const UserDataContext = createContext<UserDataContextProps | undefined>(undefine
 export const UserDataProvider: FC<UserDataProviderProps> = ({ children }) => {
   const { getStoredAuthToken } = useAuth();
   const [userData, setUserData] = useState<UserData | null>(null);
-  const [savedWords, setSavedWords] = useState<SavedWords>({}); // Initialize as an empty object
+  const [savedWords, setSavedWords] = useState<SavedWords>({});
+  const [progress, setProgress] = useState<Progress>({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,7 +56,8 @@ export const UserDataProvider: FC<UserDataProviderProps> = ({ children }) => {
         const data = await getUserData(authToken);
         if (!data) throw new Error('No user data found');
         setUserData(data as UserData);
-        setSavedWords(data?.saved_words || {}); // Ensure saved words are an object
+        setSavedWords(data?.saved_words || {});
+        setProgress(data?.progress || {});
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
@@ -95,8 +108,31 @@ export const UserDataProvider: FC<UserDataProviderProps> = ({ children }) => {
     }
   };
 
+  const getProgress = (langCode: string) => {
+    return progress[langCode];
+  };
+
+  const updateProgress = async (langCode: string, newProgress: { level: string; time: number }): Promise<void> => {
+    if (!userData) throw new Error('Cannot update progress when user data is not initialized');
+    if (!progress) throw new Error('Cannot update progress when progress is not initialized');
+
+    const updatedProgress: Progress = {
+      ...progress,
+      [langCode]: newProgress
+    };
+
+    setProgress(updatedProgress);
+    try {
+      const authToken = await getStoredAuthToken();
+      if (!authToken) throw new Error('No auth token found');
+      await patchUserData(Number(userData.id), { progress: JSON.stringify(updatedProgress) }, authToken);
+    } catch (error) {
+      console.error('Error updating user data:', error);
+    }
+  };
+
   return (
-    <UserDataContext.Provider value={{ userData, savedWords, hasSavedWord, saveWord, removeSavedWord }}>
+    <UserDataContext.Provider value={{ userData, savedWords, progress, hasSavedWord, saveWord, removeSavedWord, getProgress, updateProgress }}>
       {children}
     </UserDataContext.Provider>
   );
