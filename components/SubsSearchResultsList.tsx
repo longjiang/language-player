5d3c149e5f79cc20eacb76d59c817c722ebaa89b
Jiang Long <longjiang2005@gmail.com>
2024-07-04 18:03:36 -0700

    feat(subs-search/results-list): can filter results

--- components/SubsSearchResultsList.tsx
+++ components/SubsSearchResultsList.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedInput } from "./ThemedInput";
 import { FlatList, GestureHandlerRootView } from "react-native-gesture-handler";
@@ -42,66 +42,87 @@ export const SubsSearchResultsList = ({
   term: string;
   onSelect: (index: number) => void;
 }) => {
+  const [filteredResults, setFilteredResults] = useState(results);
+  const [searchTerm, setSearchTerm] = useState(term);
+
+  useEffect(() => {
+    filterResults(searchTerm);
+  }, [searchTerm, results]);
+
+  const filterResults = (searchTerm: string) => {
+    const filtered = results.filter((item) =>
+      item.subs_l2.some((sub: any) =>
+        sub.line.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+
+    filtered.forEach((item) => {
+      const targetLineIndex = item.subs_l2.findIndex((sub: any) => {
+        return typeof sub.line === "string" && sub.line.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+      item.targetLineIndex = targetLineIndex;
+    });
+
+    setFilteredResults(filtered);
+  };
+
   const handleSubtitlePress = (index: number) => {
     onSelect(index);
   };
 
-  results.forEach((item) => {
-    const targetLineIndex = item.subs_l2.findIndex((sub: any) => {
-      return typeof sub.line === "string" && sub.line.includes(term);
-    });
-    item.targetLineIndex = targetLineIndex;
-  });
+  const handleSearchChange = (text: string) => {
+    setSearchTerm(text);
+  };
 
   return (
     <GestureHandlerRootView style={styles.fullContainer}>
-      {results && results.length > 0 && (
-        <View style={styles.fullContainer}>
-          <ThemedInput
-            placeholder="Search..."
-            icon="magnify"
-            style={{ marginBottom: 26 }}
-          />
-          <FlatList
-            data={results}
-            renderItem={({ item, index }) => (
-              <TouchableOpacity
-                style={styles.item}
-                onPress={() => handleSubtitlePress(index)}
-              >
-                <Image
-                  source={{
-                    uri: `https://img.youtube.com/vi/${item.youtube_id}/0.jpg`,
-                  }}
-                  style={[styles.thumbnail]}
+      <View style={styles.fullContainer}>
+        <ThemedInput
+          placeholder="Search..."
+          icon="magnify"
+          style={{ marginBottom: 26 }}
+          value={searchTerm}
+          onChangeText={handleSearchChange}
+        />
+        <FlatList
+          data={filteredResults}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => handleSubtitlePress(index)}
+            >
+              <Image
+                source={{
+                  uri: `https://img.youtube.com/vi/${item.youtube_id}/0.jpg`,
+                }}
+                style={[styles.thumbnail]}
+              />
+              <ThemedText style={styles.line}>
+                <HighlightSearchTerm
+                  line={item.subs_l2[item.targetLineIndex]?.line}
+                  searchTerm={searchTerm}
                 />
-                <ThemedText style={styles.line}>
-                  <HighlightSearchTerm
-                    line={item.subs_l2[item.targetLineIndex]?.line}
-                    searchTerm={term}
-                  />
-                </ThemedText>
-              </TouchableOpacity>
-            )}
-            keyExtractor={(item, index) => index.toString()}
-          />
-        </View>
-      )}
+              </ThemedText>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
     </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
   thumbnail: {
-    width: 75, // Makes the image fill the container
-    aspectRatio: 16 / 9, // Maintains a 16:9 aspect ratio
+    width: 75,
+    aspectRatio: 16 / 9,
     borderRadius: 4,
   },
   fullContainer: {
-    flex: 1, // This will make the container fill the entire space of its parent
+    flex: 1,
   },
   highlight: {
-    color: Swatches.warning[500], // Example highlight style
+    color: Swatches.warning[500],
     fontFamily: "Nunito_700Bold",
   },
   item: {
