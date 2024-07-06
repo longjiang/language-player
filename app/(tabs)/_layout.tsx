7976f4eed0feb3ab2b3f11e8ce08877b47b13c98
Jiang Long <longjiang2005@gmail.com>
2024-07-06 12:54:10 -0700

    localization(nav): translate tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,24 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { DictionaryProvider } from '@/contexts/DictionaryContext';
-
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Typography } from '@/constants/Typography';
+import { useLanguage } from '@/contexts/LanguageContext';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const activeColor = Colors[colorScheme ?? 'light'].primaryLink;
   const tabBackgroundColor = Colors[colorScheme ?? 'light'].secondaryBackground;
+  const { t } = useLanguage();
 
   return (
     <Tabs
-      
       screenOptions={{
         tabBarActiveTintColor: activeColor,
-        tabBarStyle: { backgroundColor: tabBackgroundColor, paddingTop: 12, height:100},
-          tabBarLabelStyle: {
+        tabBarStyle: { backgroundColor: tabBackgroundColor, paddingTop: 12, height: 100 },
+        tabBarLabelStyle: {
           fontSize: Typography.fontSize.xsmall,
           fontWeight: "bold",
         },
@@ -27,7 +27,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="(media)"
         options={{
-          title: 'Media',
+          title: t('tab.media'),
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'movie' : 'movie-outline'} color={color} />
           ),
@@ -36,7 +36,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="(dictionary)"
         options={{
-          title: 'Dictionary',
+          title: t('tab.dictionary'),
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'book' : 'book-outline'} color={color} />
           ),
@@ -45,7 +45,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="(me)"
         options={{
-          title: 'Me',
+          title: t('tab.me'),
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'account' : 'account-outline'} color={color} />
           ),
