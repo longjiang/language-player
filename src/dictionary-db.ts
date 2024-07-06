9b50603b25f9712b3f1dbfdb1f3b696e8d8529c3
Jiang Long <longjiang2005@gmail.com>
2024-07-06 14:48:10 -0700

    perf(dictionary): use transaction to make inserting db records faster

--- src/dictionary-db.ts
+++ src/dictionary-db.ts
@@ -49,27 +49,33 @@ export class DictionaryDB {
     `);
   }
 
+
   async insertEntries(entries: any[], indexPronunciation = false): Promise<void> {
     const batchSize = 50;
-    for (let i = 0; i < entries.length; i += batchSize) {
-      const batch = entries.slice(i, i + batchSize);
-      const values = batch.map(entry => {
-        const pronunciationSearchString = indexPronunciation ? stripAccents(entry.pronunciation || '').toLowerCase().replace(/\s+/g, '') : '';
-        const search = `${entry.head} ${stripAccents(entry.head).toLowerCase()} ${entry.alternate || ''} ${pronunciationSearchString} ${entry.definitions.slice(0,1).join(' ').toLowerCase()}`
-        return `(${escapeSQLValue(entry.id) ? `'${escapeSQLValue(entry.id)}'` : 'NULL'}, 
-                 ${entry.hskId || 'NULL'}, 
-                 ${escapeSQLValue(entry.head) ? `'${escapeSQLValue(entry.head)}'` : 'NULL'}, 
-                 ${escapeSQLValue(entry.pronunciation) ? `'${escapeSQLValue(entry.pronunciation)}'` : 'NULL'}, 
-                 ${escapeSQLValue(entry.alternate) ? `'${escapeSQLValue(entry.alternate)}'` : 'NULL'}, 
-                 ${escapeSQLValue(entry.definitions.join(' | ')) ? `'${escapeSQLValue(entry.definitions.join(' | '))}'` : 'NULL'}, 
-                 ${entry.level || 'NULL'}, 
-                 ${escapeSQLValue(search) ? `'${escapeSQLValue(search)}'` : 'NULL'}, 
-                 ${escapeSQLValue(entry.pos) ? `'${escapeSQLValue(entry.pos)}'` : 'NULL'})`;
-      }).join(',');
-  
-      await this.db!.execAsync(`INSERT OR IGNORE INTO ${this.dbName} (id, hskId, head, pronunciation, alternate, definitions, level, search, pos) VALUES ${values}`);
-    }
+
+    // Start an exclusive transaction to ensure no other operations interfere
+    await this.db!.withExclusiveTransactionAsync(async () => {
+      for (let i = 0; i < entries.length; i += batchSize) {
+        const batch = entries.slice(i, i + batchSize);
+        const values = batch.map(entry => {
+          const pronunciationSearchString = indexPronunciation ? stripAccents(entry.pronunciation || '').toLowerCase().replace(/\s+/g, '') : '';
+          const search = `${entry.head} ${stripAccents(entry.head).toLowerCase()} ${entry.alternate || ''} ${pronunciationSearchString} ${entry.definitions.slice(0,1).join(' ').toLowerCase()}`
+          return `(${escapeSQLValue(entry.id) ? `'${escapeSQLValue(entry.id)}'` : 'NULL'}, 
+                   ${entry.hskId || 'NULL'}, 
+                   ${escapeSQLValue(entry.head) ? `'${escapeSQLValue(entry.head)}'` : 'NULL'}, 
+                   ${escapeSQLValue(entry.pronunciation) ? `'${escapeSQLValue(entry.pronunciation)}'` : 'NULL'}, 
+                   ${escapeSQLValue(entry.alternate) ? `'${escapeSQLValue(entry.alternate)}'` : 'NULL'}, 
+                   ${escapeSQLValue(entry.definitions.join(' | ')) ? `'${escapeSQLValue(entry.definitions.join(' | '))}'` : 'NULL'}, 
+                   ${entry.level || 'NULL'}, 
+                   ${escapeSQLValue(search) ? `'${escapeSQLValue(search)}'` : 'NULL'}, 
+                   ${escapeSQLValue(entry.pos) ? `'${escapeSQLValue(entry.pos)}'` : 'NULL'})`;
+        }).join(',');
+
+        await this.db!.execAsync(`INSERT OR IGNORE INTO ${this.dbName} (id, hskId, head, pronunciation, alternate, definitions, level, search, pos) VALUES ${values}`);
+      }
+    });
   }
+
 
   async createIndexes(): Promise<void> {
     await this.db!.execAsync(`CREATE INDEX IF NOT EXISTS idx_search ON ${this.dbName}(search)`);
