fa2d8526411ce581004f0ce55f284ba5f24d0e89
Jiang Long <longjiang2005@gmail.com>
2024-07-09 03:40:19 -0700

    refactor(video): move fetching logic into player context

--- components/MiniPlayer.tsx
+++ components/MiniPlayer.tsx
@@ -1,109 +1,18 @@
-import React, { useEffect } from "react";
+// @/components/MiniPlayer.tsx
+
+import React from "react";
 import { View, StyleSheet } from "react-native";
 import { useVideoPlayer } from "@/contexts/VideoPlayerContext";
 import { VideoWithTranscript } from "@/components/VideoWithTranscript";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { VideoWithTranscriptProvider } from "@/contexts/VideoWithTranscriptContext";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { useLanguage } from "@/contexts/LanguageContext";
-import { useAuth } from "@/contexts/AuthContext";
-import { addToWatchHistory } from "@/src/api/directus/user-watch-history";
-import { getBestL1Subs, getBestL2Subs } from "@/src/api/python/video";
-import { useDictionary } from "@/contexts/DictionaryContext";
-import { getTokenizerCacheForVideo } from "@/src/api/python/video";
-import { getVideosByL2Code } from "@/src/api/directus/youtube-video";
 
 export const MiniPlayer = () => {
   const primaryBackgroundColor = useThemeColor({}, "primaryBackground");
   const primaryBrandColor = useThemeColor({}, "primaryBrand");
 
-  const { videoPlayerState, setVideoAndQueue } = useVideoPlayer();
-  const { l1Lang, l2Lang } = useLanguage();
-  const { getStoredAuthToken } = useAuth();
-  const { tokenizer } = useDictionary();
-
-  useEffect(() => {
-    const fetchVideoDetails = async () => {
-      if (!videoPlayerState.video?.youtube_id || !l2Lang || !l1Lang) return;
-
-      try {
-        // Fetch video details
-        const videos = await getVideosByL2Code(l2Lang, true, {
-          filter: {
-            youtube_id: {
-              eq: videoPlayerState.video.youtube_id,
-            },
-          },
-        });
-        let updatedVideo = videos?.length ? videos[0] : { ...videoPlayerState.video };
-
-        // Fetch L2 subtitles if they don't exist
-        if (!updatedVideo.subs_l2?.length) {
-          try {
-            const l2Subs = await getBestL2Subs(updatedVideo.youtube_id, l2Lang.code);
-            updatedVideo.subs_l2 = l2Subs || [];
-          } catch (error) {
-            console.error("Failed to fetch L2 subs", error);
-          }
-        }
-
-        // Fetch L1 subtitles if they don't exist
-        if (!updatedVideo.subs_l1?.length) {
-          try {
-            const l1Subs = await getBestL1Subs(updatedVideo.youtube_id, l1Lang.code, l2Lang.code);
-            updatedVideo.subs_l1 = l1Subs || [];
-          } catch (error) {
-            console.error("Failed to fetch L1 subs", error);
-          }
-        }
-
-        // If the video has a tv_show id, load the show episodes
-        let updatedQueue = [...videoPlayerState.queue];
-        if (updatedVideo.tv_show) {
-          const showEpisodes = await getVideosByL2Code(l2Lang, false, {
-            filter: {
-              tv_show: {
-                eq: updatedVideo.tv_show,
-              },
-            },
-            sort: ['title'],
-          });
-
-          // Find the index of the current video in the show episodes
-          const currentIndex = showEpisodes.findIndex(ep => ep.youtube_id === updatedVideo.youtube_id);
-          
-          // Queue up the next episodes
-          if (currentIndex !== -1 && currentIndex < showEpisodes.length - 1) {
-            updatedQueue = Array.from(new Set(showEpisodes.slice(currentIndex + 1)));
-          }
-        }
-
-        // Update video player state
-        setVideoAndQueue(updatedVideo, updatedQueue);
-
-        // Add to watch history
-        if (updatedVideo.id) {
-          const authToken = await getStoredAuthToken();
-          if (authToken) {
-            await addToWatchHistory(l2Lang.id, Number(updatedVideo.id), 0, authToken);
-          }
-        }
-        // Fetch and load tokenizer cache
-        try {
-          const tokenizerCache = await getTokenizerCacheForVideo(updatedVideo.id, l2Lang.code);
-          if (tokenizerCache && tokenizer) {
-            tokenizer.loadCache(tokenizerCache);
-          }
-        } catch (error) {
-          console.error("Failed to fetch and load tokenizer cache", error);
-        }
-      } catch (error) {
-        console.error("Failed to fetch video details", error);
-      }
-    };
-
-    fetchVideoDetails();
-  }, [videoPlayerState.video?.youtube_id, l1Lang, l2Lang]);
+  const { videoPlayerState } = useVideoPlayer();
 
   if (!videoPlayerState.video) {
     return null;
--- contexts/VideoPlayerContext.tsx
+++ contexts/VideoPlayerContext.tsx
@@ -1,7 +1,13 @@
-// @/contexts/VideoPlayerContext
+// @/contexts/VideoPlayerContext.tsx
 
-import React, { createContext, useContext, useState, ReactNode, Dispatch, SetStateAction } from 'react';
-import { YouTubeVideo } from "@/types/videoTypes"
+import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import { YouTubeVideo } from "@/types/videoTypes";
+import { useLanguage } from "@/contexts/LanguageContext";
+import { useAuth } from "@/contexts/AuthContext";
+import { useDictionary } from "@/contexts/DictionaryContext";
+import { addToWatchHistory } from "@/src/api/directus/user-watch-history";
+import { getBestL1Subs, getBestL2Subs, getTokenizerCacheForVideo } from "@/src/api/python/video";
+import { getVideosByL2Code } from "@/src/api/directus/youtube-video";
 
 type VideoPlayerState = {
   isMini: boolean;
@@ -15,7 +21,7 @@ type VideoPlayerContextType = {
   playPrevious: () => void;
   closePlayer: () => void;
   minimizePlayer: () => void;
-  maximizePlayer:  () => void;
+  maximizePlayer: () => void;
   setVideoAndQueue: (video: YouTubeVideo, queue: YouTubeVideo[]) => void;
 };
 
@@ -25,56 +31,41 @@ const initialVideoPlayerState: VideoPlayerState = {
   queue: [],
 };
 
-const VideoPlayerContext = createContext<VideoPlayerContextType>({
-  videoPlayerState: initialVideoPlayerState,
-  closePlayer: function (): void {
-    throw new Error('Function not implemented.');
-  },
-  minimizePlayer: function (): void {
-    throw new Error('Function not implemented.');
-  },
-  maximizePlayer: function (): void {
-    throw new Error('Function not implemented.');
-  },
-  playNext: function (): void {
-    throw new Error('Function not implemented.');
-  },
-  playPrevious: function (): void {
-    throw new Error('Function not implemented.');
-  },
-  setVideoAndQueue: function (): void {
-    throw new Error('Function not implemented.');
-  },
-});
+const VideoPlayerContext = createContext<VideoPlayerContextType | undefined>(undefined);
 
-export const useVideoPlayer = () => useContext(VideoPlayerContext);
+export const useVideoPlayer = () => {
+  const context = useContext(VideoPlayerContext);
+  if (context === undefined) {
+    throw new Error('useVideoPlayer must be used within a VideoPlayerProvider');
+  }
+  return context;
+};
 
 export const VideoPlayerProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [videoPlayerState, setVideoPlayerState] = useState(initialVideoPlayerState);
-  
-  // Helper functions to modify state
+  const { l1Lang, l2Lang } = useLanguage();
+  const { getStoredAuthToken } = useAuth();
+  const { tokenizer } = useDictionary();
+
   const closePlayer = () => setVideoPlayerState({ isMini: false, video: undefined, queue: [] });
-  const minimizePlayer = () => {
-    setVideoPlayerState(prev => ({ ...prev, isMini: true }))
-  };
-  
+  const minimizePlayer = () => setVideoPlayerState(prev => ({ ...prev, isMini: true }));
   const maximizePlayer = () => setVideoPlayerState(prev => ({ ...prev, isMini: false }));
 
   const playNext = () => {
-    const currentIndex = videoPlayerState.queue.findIndex(v => v.youtube_id === videoPlayerState.video.youtube_id);
+    const currentIndex = videoPlayerState.queue.findIndex(v => v.youtube_id === videoPlayerState.video?.youtube_id);
     const nextIndex = currentIndex + 1;
     if (nextIndex < videoPlayerState.queue.length) {
       const nextVideo = videoPlayerState.queue[nextIndex];
-      setVideoPlayerState({ ...videoPlayerState, video: nextVideo });
+      setVideoPlayerState(prev => ({ ...prev, video: nextVideo }));
     }
   };
 
   const playPrevious = () => {
-    const currentIndex = videoPlayerState.queue.findIndex(v => v.youtube_id === videoPlayerState.video.youtube_id);
+    const currentIndex = videoPlayerState.queue.findIndex(v => v.youtube_id === videoPlayerState.video?.youtube_id);
     const prevIndex = currentIndex - 1;
     if (prevIndex >= 0) {
       const previousVideo = videoPlayerState.queue[prevIndex];
-      setVideoPlayerState({ ...videoPlayerState, video: previousVideo });
+      setVideoPlayerState(prev => ({ ...prev, video: previousVideo }));
     }
   };
 
@@ -87,6 +78,93 @@ export const VideoPlayerProvider: React.FC<{ children: ReactNode }> = ({ childre
     }));
   };
 
+  const fetchVideoDetails = async (youtubeId: string) => {
+    if (!youtubeId || !l2Lang || !l1Lang) return;
+
+    try {
+      // Fetch video details
+      const videos = await getVideosByL2Code(l2Lang, true, {
+        filter: {
+          youtube_id: {
+            eq: youtubeId,
+          },
+        },
+      });
+      let updatedVideo = videos?.length ? videos[0] : { ...videoPlayerState.video, youtube_id: youtubeId };
+
+      // Fetch L2 subtitles if they don't exist
+      if (!updatedVideo.subs_l2?.length) {
+        try {
+          const l2Subs = await getBestL2Subs(updatedVideo.youtube_id, l2Lang.code);
+          updatedVideo.subs_l2 = l2Subs || [];
+        } catch (error) {
+          console.error("Failed to fetch L2 subs", error);
+        }
+      }
+
+      // Fetch L1 subtitles if they don't exist
+      if (!updatedVideo.subs_l1?.length) {
+        try {
+          const l1Subs = await getBestL1Subs(updatedVideo.youtube_id, l1Lang.code, l2Lang.code);
+          updatedVideo.subs_l1 = l1Subs || [];
+        } catch (error) {
+          console.error("Failed to fetch L1 subs", error);
+        }
+      }
+
+      // If the video has a tv_show id, load the show episodes
+      let updatedQueue = [...videoPlayerState.queue];
+      if (updatedVideo.tv_show) {
+        const showEpisodes = await getVideosByL2Code(l2Lang, false, {
+          filter: {
+            tv_show: {
+              eq: updatedVideo.tv_show,
+            },
+          },
+          sort: ['title'],
+        });
+
+        // Find the index of the current video in the show episodes
+        const currentIndex = showEpisodes.findIndex(ep => ep.youtube_id === updatedVideo.youtube_id);
+        
+        // Queue up the next episodes
+        if (currentIndex !== -1 && currentIndex < showEpisodes.length - 1) {
+          updatedQueue = Array.from(new Set(showEpisodes.slice(currentIndex + 1)));
+        }
+      }
+
+      // Update video player state
+      setVideoAndQueue(updatedVideo, updatedQueue);
+
+      // Add to watch history
+      if (updatedVideo.id) {
+        const authToken = await getStoredAuthToken();
+        if (authToken) {
+          await addToWatchHistory(l2Lang.id, Number(updatedVideo.id), 0, authToken);
+        }
+      }
+
+      // Fetch and load tokenizer cache
+      try {
+        const tokenizerCache = await getTokenizerCacheForVideo(updatedVideo.id, l2Lang.code);
+        if (tokenizerCache && tokenizer) {
+          tokenizer.loadCache(tokenizerCache);
+        }
+      } catch (error) {
+        console.error("Failed to fetch and load tokenizer cache", error);
+      }
+    } catch (error) {
+      console.error("Failed to fetch video details", error);
+    }
+  };
+
+  // New useEffect hook to fetch video details when the video changes
+  useEffect(() => {
+    if (videoPlayerState.video?.youtube_id) {
+      fetchVideoDetails(videoPlayerState.video.youtube_id);
+    }
+  }, [videoPlayerState.video?.youtube_id, l1Lang, l2Lang]);
+
   const value = {
     videoPlayerState,
     playNext,
@@ -96,8 +174,6 @@ export const VideoPlayerProvider: React.FC<{ children: ReactNode }> = ({ childre
     maximizePlayer,
     setVideoAndQueue,
   };
-
-  // console.log('vp Context: videoPlayerState.queue=', videoPlayerState.queue.length)
 
   return (
     <VideoPlayerContext.Provider value={value}>
