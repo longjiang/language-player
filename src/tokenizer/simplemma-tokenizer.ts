cd5b1da8374593e3e40adc114559b2c0783ac1bd
Jiang Long <longjiang2005@gmail.com>
2024-07-09 00:01:50 -0700

    fix(tokenization): English subs do not have spaces

--- src/tokenizer/simplemma-tokenizer.ts
+++ src/tokenizer/simplemma-tokenizer.ts
@@ -1,8 +1,50 @@
+// @/src/tokenizer/simplemma-tokenizer.ts
+
 import { Token, Lemma } from '@/src/tokenizer';
 
+export const addSpaceTokens = (tokens: Token[], text: string): Token[] => {
+  let newTokens: Token[] = [];
+  let prevWasPunctuation = false;
+  let insideQuote = false;
+
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+    const word = token.text;
+
+    if (word === '"' || word === "'") {
+      if (insideQuote) {
+        newTokens.push(token);
+        insideQuote = false;
+      } else {
+        if (newTokens.length > 0 && !prevWasPunctuation) {
+          newTokens.push({ text: " ", pos: undefined, lemmas: [], pronunciation: undefined });
+        }
+        newTokens.push(token);
+        insideQuote = true;
+      }
+      prevWasPunctuation = true;
+    } else if (word === "," || word === "." || word === ":" || word === ";") {
+      newTokens.push(token);
+      prevWasPunctuation = true;
+    } else {
+      if (newTokens.length > 0 && !prevWasPunctuation && !insideQuote) {
+        newTokens.push({ text: " ", pos: undefined, lemmas: [], pronunciation: undefined });
+      }
+      newTokens.push(token);
+      prevWasPunctuation = false;
+    }
+  }
+
+  return newTokens;
+}
 
 export const normalizeTokens = (tokens: Token[], text: string): Token[] => {
-  return tokens.map(normalizeToken);
+  // console.log('🫐', tokens)
+  let normalizedTokens = tokens.map(normalizeToken);
+  // console.log('🍋', normalizedTokens);
+  normalizedTokens = addSpaceTokens(normalizedTokens, text);
+  // console.log('🍒', normalizedTokens);
+  return normalizedTokens;
 }
 
 function normalizeToken(token: any): Token {
