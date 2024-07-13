d1d042abdb826f18b09a3b734f4cbdb834984f4d
Jiang Long <longjiang2005@gmail.com>
2024-07-13 03:09:33 -0700

    feat(login): forget password page

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,7 @@
 // @/app/login.tsx
 
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Alert, Linking } from 'react-native';
 import { ThemedText, ThemedButton, ThemedInput, ThemedScreen } from '@/components';
 import { router } from 'expo-router';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -41,6 +41,17 @@ const LoginScreen = () => {
         }
     };
 
+    const handleForgotPassword = async () => {
+        const url = 'https://languageplayer.io/forgot-password/';
+        const supported = await Linking.canOpenURL(url);
+
+        if (supported) {
+            await Linking.openURL(url);
+        } else {
+            Alert.alert("Error", "Cannot open the forgot password page");
+        }
+    };
+
     return (
         <ThemedScreen
             title={t('title.login')}
@@ -67,19 +78,12 @@ const LoginScreen = () => {
 
             <ThemedButton title={t('title.login')} onPress={onLoginPress} disabled={loading} />
 
-            <TouchableOpacity style={styles.textButton}>
+            <TouchableOpacity style={styles.textButton} onPress={handleForgotPassword}>
                 <ThemedText>{t('msg.forgot_password')}</ThemedText>
             </TouchableOpacity>
 
-            {/* <ThemedText style={styles.orText}>Or login with:</ThemedText>
-
-            <View style={styles.socialButtons}>
-                <ThemedButton type="neutral" size="large" style={styles.socialButton} trailingIcon={<Icon name="google" />} onPress={() => router.navigate('/')} />
-                <ThemedButton type="neutral" size="large" style={styles.socialButton} trailingIcon={<Icon name="apple" />} onPress={() => router.navigate('/')} />
-                <ThemedButton type="neutral" size="large" style={styles.socialButton} trailingIcon={<Icon name="facebook" />} onPress={() => router.navigate('/')} />
-            </View> */}
-
-            <ThemedText style={{ textAlign: 'left', marginTop: 26 }} type="subtitle">{t('msg.dont_have_an_account')} <Link href='/register' style={{ color: useThemeColor({}, 'primaryLink'), fontWeight: 'bold' }}>{t('title.register')}</Link>
+            <ThemedText style={{ textAlign: 'left', marginTop: 26 }} type="subtitle">
+                {t('msg.dont_have_an_account')} <Link href='/register' style={{ color: useThemeColor({}, 'primaryLink'), fontWeight: 'bold' }}>{t('title.register')}</Link>
             </ThemedText>
         </ThemedScreen>
     );
@@ -93,21 +97,6 @@ const styles = StyleSheet.create({
         marginTop: 10,
         alignSelf: 'flex-start',
     },
-    orText: {
-        textAlign: 'center',
-        color: 'white',
-        marginTop: 20,
-        marginBottom: 10,
-    },
-    socialButton: {
-        marginHorizontal: 4,
-        marginBottom: 10,
-        flex: 1,
-    },
-    socialButtons: {
-        flexDirection: 'row',
-        marginTop: 10,
-    }
 });
 
 export default LoginScreen;
