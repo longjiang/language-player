38efece680d90efc54752fd9c5302322552ec612
Jiang Long <longjiang2005@gmail.com>
2026-07-22 16:10:50 -0700

    chore: remove reader screen registration from reading _layout (replaced by index)

--- apps/mobile-v2/app/(tabs)/(reading)/_layout.tsx
+++ apps/mobile-v2/app/(tabs)/(reading)/_layout.tsx
@@ -4,7 +4,6 @@ export default function ReadingLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="reader" />
       <Stack.Screen name="web-reader" />
       <Stack.Screen name="epub" />
     </Stack>
